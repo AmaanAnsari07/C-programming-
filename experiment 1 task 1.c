3982c9f5d9d0df7a42f66a92aa77cafcb2679894
AmaanAnsari07 <khansawaqar9370@gmail.com>
2024-11-29 15:08:46 +0530

    Add files via upload

---
+++ experiment 1 task 1.c
@@ -0,0 +1,12 @@
+ # include <stdio.h>
+ int main () {
+
+     printf(" Full Name    : Amaan Ansari\n ");
+     printf("Age          : 17 \n");
+     printf(" Class        : AIDS \n");
+     printf (" Division     : A \n");
+
+     return 0;
+
+
+     }
